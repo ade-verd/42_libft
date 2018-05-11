b170d9aaf995afd5d023408ed3ac906b2fcee703
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-05-11 11:18:50 +0200

    strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 09:13:43 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/05/11 11:16:01 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/05/11 11:18:41 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ void			ft_freetab_strsplit(char **tab)
 	int		i;
 
 	i = 0;
-	while (*tab && tab[i])
+	while (*tab && *tab[i])
 	{
 		printf("del tab[%d]\n", i);
 		ft_memdel((void**)&tab[i]);
