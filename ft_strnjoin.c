69c8e6c39413fd642a343e3f40caaadd47b246fa
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-06-26 12:39:15 +0200

    test strnjoin

--- ft_strnjoin.c
+++ ft_strnjoin.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 17:14:29 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/06/26 12:36:50 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/06/26 12:38:54 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ char	*ft_strnjoin(const char *s1, const char *s2, size_t len)
 		return (NULL);
 	s = ft_strnew(ft_strlen(s1) + len + 1);
 	join = s;
-	while (s1)
+	while (*s1)
 		*s++ = *s1++;
-	while (s2 && len--)
+	while (*s2 && len--)
 		*s++ = *s2++;
 	return (join);
 }
