72be4366edc506bef24026e00f4fc7cb15ac9121
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-06-26 12:37:12 +0200

    test strnjoin

--- ft_strnjoin.c
+++ ft_strnjoin.c
@@ -6,13 +6,13 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 17:14:29 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/05/25 14:11:14 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/06/26 12:36:50 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnjoin(char const *s1, char const *s2, size_t len)
+char	*ft_strnjoin(const char *s1, const char *s2, size_t len)
 {
 	char	*s;
 	char	*join;
@@ -21,9 +21,9 @@ char	*ft_strnjoin(char const *s1, char const *s2, size_t len)
 		return (NULL);
 	s = ft_strnew(ft_strlen(s1) + len + 1);
 	join = s;
-	while (*s1)
+	while (s1)
 		*s++ = *s1++;
-	while (*s2 && len--)
+	while (s2 && len--)
 		*s++ = *s2++;
 	return (join);
 }
