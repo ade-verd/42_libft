7969de8194f6758c31a791bf3cd3010f4ea5c6ba
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-07-25 12:33:22 +0200

    ft_file_exists uses open

--- ft_file_exists.c
+++ ft_file_exists.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/25 12:20:05 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/07/25 12:27:01 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/07/25 12:33:05 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_file_exists(char *path)
 {
 	int		fd;
 
-	if ((fd = ft_open_fd(path, O_RDONLY, 0)) != -1)
+	if ((fd = open(path, O_RDONLY)) != -1)
 	{
 		ft_close_fd(fd);
 		return (1);
