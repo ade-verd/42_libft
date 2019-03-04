435cbb6c29d74c59d94d72f9bb5939967852d527
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2019-03-04 15:40:06 +0100

    fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 17:46:00 by ade-verd          #+#    #+#             */
-/*   Updated: 2019/03/04 15:16:45 by ade-verd         ###   ########.fr       */
+/*   Updated: 2019/03/04 15:39:37 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char			*ft_strtrim(char const *s)
 	char	*trim;
 	int		i;
 	int		a;
-	int		end;
+	int		z;
 	int		diff;
 
 	i = 0;
@@ -27,13 +27,13 @@ char			*ft_strtrim(char const *s)
 		return (NULL);
 	while (s[a] && (s[a] == ' ' || s[a] == '\t' || s[a] == '\n'))
 		a++;
-	end = ft_strlen(s) - 1;
-	while (s[end] && (s[end] == ' ' || s[end] == '\t' || s[end] == '\n'))
-		end--;
-	diff = end - a + 1;
-	if (!diff || (trim = (char*)malloc(sizeof(char) * (diff + 1))) == NULL)
+	z = ft_strlen(s) - 1;
+	while (z >= 0 && s[z] && (s[z] == ' ' || s[z] == '\t' || s[z] == '\n'))
+		z--;
+	diff = (z < a ? 1 : z - a + 1);
+	if ((trim = (char*)malloc(sizeof(char) * (diff + 1))) == NULL)
 		return (NULL);
-	while (a <= end)
+	while (a <= z)
 	{
 		trim[i++] = s[a];
 		a++;
