270ac605ffb9b402d24d4cbfd873b8295679c51a
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2019-03-04 18:43:32 +0100

    GNL rm MALLCHECK

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 16:15:05 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/05/25 13:21:26 by ade-verd         ###   ########.fr       */
+/*   Updated: 2019/03/04 18:43:07 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@
 # include "libft.h"
 
 # define BUFF_SIZE 42
-# define MALLCHECK(x) if (!x) return (-1);
 
-int				get_next_line(const int fd, char **line);
+int	get_next_line(const int fd, char **line);
 
 #endif
